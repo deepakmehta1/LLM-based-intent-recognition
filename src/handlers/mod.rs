2c93fd0deb8e256e423fdc55650800860bba3b03
deepakmehta1 <deepak.deep40@yahoo.com>
2024-10-09 12:07:38 +0530

    cleaned up unused stuffs

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -6,10 +6,3 @@ pub mod intents;
 pub use prompts::joke_prompt;
 pub use responses::get_response;
 pub use history::{save_message, load_history, Message};
-pub use intents::{
-    handle_informative_intent,
-    handle_empathetic_intent,
-    handle_task_oriented_intent,
-    handle_others_intent,
-    handle_intent,
-};
